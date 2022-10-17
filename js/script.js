031c1d793efe3d48d11935db593d39b6f4cc0d93
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-10-16 20:08:32 -0400

    work inprogress

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+// array list that will contain answer
+// let answerOptions = ['you will not figure out the piece of code today. use ing 4 answers, hope to find some, search for them']
+
+
+
+// create random number generator
+let randomNum;
+// = Math.floor(Math.random()*answerOptionLength);
+
+console.log(randomNum)
+
+
+let answerOptionLength = answerOption.length;
+function ask();
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', function ask(){
+    randomNum = Math.floor(Math.random()*answerOptionLength);
+})
+
+
+// use parse to get the random answers or find API 
